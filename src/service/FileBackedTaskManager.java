013e36a2ef76064465f41687b9069552e7757ef6
Maxim <Maxiiiiiiiiim@yandex.ru>
2024-09-28 17:51:50 +0300

    +

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -3,25 +3,17 @@ package service;
 import model.Epic;
 import model.SubTask;
 import model.Task;
-import java.io.IOException;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import java.io.File;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
-    private Path file;
-
     private static final String PATH_TO_FILE = "/resources/path/to/file";
+    private Path file;
 
     public FileBackedTaskManager(Path file) {
         this.file = file;
@@ -32,6 +24,19 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         }
     }
 
+    public static FileBackedTaskManager loadFromFile(Path file) {
+        FileBackedTaskManager taskManager = new FileBackedTaskManager(Path.of(PATH_TO_FILE));
+        try (BufferedReader br = Files.newBufferedReader(file)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Task task = CSVFormatter.fromString(line);
+                taskManager.createTask(task);
+            }
+        } catch (IOException e) {
+            throw new ManagerSaveException("Error loading from file", e);
+        }
+        return taskManager;
+    }
 
     @Override
     public Task createTask(Task task) {
@@ -87,13 +92,11 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         save();
     }
 
-
     @Override
     public void updateSubTask(SubTask subTask) {
         super.updateSubTask(subTask);
         save();
     }
-
 
     public void save() {
         try (BufferedWriter bw = Files.newBufferedWriter(file)) {
@@ -104,19 +107,5 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         } catch (IOException e) {
             throw new ManagerSaveException("Error saving to file", e);
         }
-    }
-
-    public static FileBackedTaskManager loadFromFile(Path file) {
-        FileBackedTaskManager taskManager = new FileBackedTaskManager(Path.of(PATH_TO_FILE));
-        try (BufferedReader br = Files.newBufferedReader(file)) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                Task task = CSVFormatter.fromString(line);
-                taskManager.createTask(task);
-            }
-        } catch (IOException e) {
-            throw new ManagerSaveException("Error loading from file", e);
-        }
-        return taskManager;
     }
 }
